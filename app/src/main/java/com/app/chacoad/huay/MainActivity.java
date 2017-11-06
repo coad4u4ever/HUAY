dc3089fb87ecd43904762adf37cf2eb798c7c646
coad4u4ever <mynamecoad@gmail.com>
2017-11-07 00:29:25 +0700

    add number activity and layout for accepting user number. add function to move to number activity when click on the button. :fire:

--- app/src/main/java/com/app/chacoad/huay/MainActivity.java
+++ app/src/main/java/com/app/chacoad/huay/MainActivity.java
@@ -1,15 +1,18 @@
 package com.app.chacoad.huay;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.app.chacoad.huay.Util.DayDate;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView mainActivityDate = null;
     Button mainActivityButton1 = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +24,19 @@ public class MainActivity extends AppCompatActivity {
         String nextHuayDate = getString(R.string.th_day_huay_tx_1) + " " + dayDate.getNextHuayDate();
         mainActivityDate.setText(dateFormat);
         mainActivityButton1.setText(nextHuayDate);
+
+
+        mainActivityButton1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.main_activity_button1:
+                Intent intent = new Intent(this, NumberActivity.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
---
+++ app/src/main/java/com/app/chacoad/huay/NumberActivity.java
@@ -0,0 +1,17 @@
+package com.app.chacoad.huay;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Admin on 7/11/2560.
+ */
+
+public class NumberActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number);
+
+    }
+}
